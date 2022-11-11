ac5b91c0cb0d0cf6225db9731ecdf123b5367d73
Jazze7 <jassimsm9@gmail.com>
2022-11-11 19:56:10 +0530

    loginpage completed

--- findyourkart/settings.py
+++ findyourkart/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'findyourkart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'findyourkart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fyk',
+        'USER': 'jazzi',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -116,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR, 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
+# from posts.models import Author
+
+
+# from users.forms import UserForm
+# from main.functions import generate_form_errors
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+
+                return HttpResponseRedirect("/")
+
+        context = {
+            "title": "Login",
+            "error": True,
+            "message": "Invalid username or password"
+        }
+        return render(request, "users/login.html", context)
+    else:
+        context = {
+            "title": "Login",
+
+        }
+        return render(request, "users/login.html", context)
