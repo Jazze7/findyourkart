a3ee17bc2b8faac5a4156664b91c3c5b158b0eb9
Jazze7 <jassimsm9@gmail.com>
2022-11-15 17:17:19 +0530

    create function added successfully

--- web/views.py
+++ web/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
+
+
 def index(request):
-    context={}
-    return render(request,"web/index.html",context=context)
+    Products = Product.objects.filter(is_deleted=False)
+    context = {
+        "title": "Find Your Kart",
+        "products": Products
+    }
+    return render(request, "web/index.html", context=context)
+   
