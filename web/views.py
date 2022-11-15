a4bca0d59c5c9206c425b3db2e614a25f63968db
Jazze7 <jassimsm9@gmail.com>
2022-11-15 18:04:51 +0530

    added filter function

--- web/views.py
+++ web/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category, Brand
 
 # Create your views here.
 
 
 def index(request):
     Products = Product.objects.filter(is_deleted=False)
+
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
+
+    search_brand = request.GET.getlist("brand")
+    if search_brand:
+        products = Product.objects.filter(author__in=search_brand)
+
+    search_categories = request.GET.getlist("category")
+    if search_categories:
+        products = Product.objects.filter(
+            categories__in=search_categories).distinct()
     context = {
         "title": "Find Your Kart",
-        "products": Products
+        "products": Products,
+        "categories": categories,
+        "brands": brands
     }
     return render(request, "web/index.html", context=context)
-   
